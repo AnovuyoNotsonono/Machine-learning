e71029b18cbdbd0da62cdb9090219f76a5fc4c7f
AnovuyoNotsonono <AnovuyoNotsonono@github.com>
2025-11-26 22:24:20 +0200

    Wrapped the code in the training sript inside main() to ensure that training only runs when the main train.py script is run

--- Tumor-Detection/Code/train.py
+++ Tumor-Detection/Code/train.py
@@ -6,40 +6,43 @@ from tensorflow.keras.callbacks import EarlyStopping
 from model import define_model
 import pickle
 
-# Specifying the rules and parameters
 
-train_datagen = ImageDataGenerator(rescale=1./255, rotation_range=20, horizontal_flip=True,
+def get_training_history():
+    """Optional function to load previously saved history inside another function"""
+    with open("history.pkl", "rb") as f:
+        return pickle.load(f)
+
+if __name__ == "__main__":
+
+    # Specifying the rules and parameters
+
+    train_datagen = ImageDataGenerator(rescale=1./255, rotation_range=20, horizontal_flip=True,
                                   vertical_flip=True,shear_range=0.2,zoom_range=0.2,
                                   width_shift_range=0.2, height_shift_range=0.2,)
 
-test_datagen = ImageDataGenerator(rescale=1./255)
+    test_datagen = ImageDataGenerator(rescale=1./255)
 
-train_datagenerator = train_datagen.flow_from_directory('../train/',target_size=(50,50), batch_size=32,
+    train_datagenerator = train_datagen.flow_from_directory('../train/',target_size=(50,50), batch_size=32,
                                                        class_mode='binary')
 
-test_datagenerator = test_datagen.flow_from_directory('../test/',target_size=(50,50), batch_size=32,
+    test_datagenerator = test_datagen.flow_from_directory('../test/',target_size=(50,50), batch_size=32,
                                                       class_mode='binary')
 
-# Apply EarlyStopping regularization to stop model from ovefitting
-early_stop = EarlyStopping(
-    monitor='val_loss',
-    patience=5,
-    restore_best_weights=True
-)
+    # Apply EarlyStopping regularization to stop model from ovefitting
+    early_stop = EarlyStopping(
+        monitor='val_loss',
+        patience=5,
+        restore_best_weights=True)
 
-# Call the model
-model = define_model()
+    # Call the model
+    model = define_model()
 
-#Train the model
-history = model.fit(train_datagenerator, epochs=100, validation_data=test_datagenerator, callbacks=[early_stop])
+    #Train the model
+    history = model.fit(train_datagenerator, epochs=100, validation_data=test_datagenerator, callbacks=[early_stop])
+   #Save the model
+    model.save("Tumor_detector.keras")
 
-model.save("Tumor_detector.keras")
+  # Save history object to a file so it can be imported later
 
-# Save history object to a file so it can be imported later
-
-with open("history.pkl", "wb") as f:
-    pickle.dump(history.history, f)
-
-# Optional: return history if running from another script
-def get_training_history():
-    return history.history
+    with open("history.pkl", "wb") as f:
+        pickle.dump(history.history, f)
