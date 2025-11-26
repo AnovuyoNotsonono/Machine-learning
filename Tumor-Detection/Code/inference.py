e4a440fa24132a9c04b341b574bf67a65805530b
AnovuyoNotsonono <AnovuyoNotsonono@github.com>
2025-11-26 21:10:08 +0200

    Edited the 'inference.py' script to load the saved model and imported the numpy ImageDatagenerator libraries

--- Tumor-Detection/Code/inference.py
+++ Tumor-Detection/Code/inference.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python
 
+import numpy as np
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.models import load_model
+
+model = load_model("Tumor_detector.keras", compile=False)
+
 def predict_image(model, image_path):
     img = image.load_img(image_path, target_size=(50, 50))
     img_array = image.img_to_array(img) / 255.0
